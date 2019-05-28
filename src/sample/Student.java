5c646b84ae9a972193945b49de80408b35e3a6ea
emily <tayem@sheridancollege.ca>
2019-05-28 12:33:08 -0400

    commit1

---
+++ src/sample/Student.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author Em
+ */
+public class Student {
+    private int id;
+    private String name;
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
+    
+}
